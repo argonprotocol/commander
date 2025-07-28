77b8054b2e1f5e453b8619d2c2f949f608a3797e
Caleb Clark <calebjclark@gmail.com>
2025-07-28 13:54:23 -0500

    fix: create_shasum.sh now excludes some files + tauri only copies relevant resource files

--- scripts/create_shasum.sh
+++ scripts/create_shasum.sh
@@ -5,4 +5,4 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-find "$1" -type f -print0 | LC_ALL=C sort -z | xargs -0 sha256sum | awk '{ print $1 }' | sha256sum | awk '{ print $1 }'
+find "$1" -type f -not -path "*/node_modules/*" -not -path "*/__test__/*" -not -name "vitest.config.ts" -print0 | LC_ALL=C sort -z | xargs -0 sha256sum | awk '{ print $1 }' | sha256sum | awk '{ print $1 }'
